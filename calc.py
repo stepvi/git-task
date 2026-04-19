b90b54b3c8bf4015edde0893466315f069f8ed1d
Виталий Степанов <stepanov@bk.ru>
2026-04-19 20:28:05 +0300

    fix bugs and add sqrt

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
-# Автор: ...
+# Автор: Виталий Степанов
+import math
 
 def add(a, b):
     return a + b
@@ -10,8 +11,10 @@ def subtract(a, b):
 
 def multiply(a, b):
     # TODO: не реализовано
-    return None
+    return  a * b
 
+def sqrt(x):
+    return math.sqrt(x)
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
